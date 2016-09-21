4c9d58042d4a0fdbfdac4b816190bddf06798e44
Miroslav Volovec <miroslav.volovec@gmail.com>
2016-09-21 17:24:47 +0200

    jsx support fixed

--- client/app.jsx
+++ client/app.jsx
@@ -1,1 +1,18 @@
-import React from 'react'
+import React, {Component} from 'react'
+import {render} from 'react-dom'
+
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        <h4>Hello world</h4>
+      </div>
+    )
+  }
+}
+
+let element = document.querySelector('#content')
+console.log(element)
+
+
+render(<Main />, element)
